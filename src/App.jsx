91137e5ef0e18d0671848bdb232f4d94ac51e7df
Avdheshbhardwajz <avdheshbhardwaj2004@gmail.com>
2024-07-25 20:26:49 +0530

    added template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-gradient-to-r from-slate-900 to-slate-700 font-poppins w-[100vw] h-[100vh] text-center flex flex-col justify-center items-center">
+      <h1 className="text-8xl  font-extrabold  text-white">
+        Your Portfolio Will be Here
+      </h1>
+    </div>
+  );
+};
+
+export default App;

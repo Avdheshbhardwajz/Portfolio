3a3857f7f9be3f2602af417aa6e6a00da07b7a9f
Avdheshbhardwajz <avdheshbhardwaj2004@gmail.com>
2024-07-26 06:14:56 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="bg-gradient-to-r from-slate-900 to-slate-700 font-poppins w-[100vw] h-[100vh] text-center flex flex-col justify-center items-center">
-      <h1 className="text-8xl  font-extrabold  text-white">
+    <div className="bg-gradient-to-r from-slate-900 to-slate-700 font-poppins w-[100vw] h-[100vh] text-center flex flex-col justify-center items-center box-border m-0">
+      <h1 className="sm:text-8xl text-6xl  font-extrabold  text-white">
         Your Portfolio Will be Here
       </h1>
     </div>
